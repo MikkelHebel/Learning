045837628b6b7f9ff68e2c70c66119a50938c4a5
MikkelHebel <mikkelhebelv2@gmail.com>
2023-08-19 15:05:10 +0200

    Made bubble sort! :D

--- Sorting Algorithms/Bubble_Sort.py
+++ Sorting Algorithms/Bubble_Sort.py
@@ -3,10 +3,20 @@
 
 array = [1, 5, 2, 6, 3, 7, 10, 8, 9, 4]
 
-def sorting(list):
-    print("[INFO]: Starting sorting process with array:")
-    print(list)
+def bubble(list):
+    sorted = False
 
-    return sorted
+    while sorted == False:
+        sorted = True
+        # This for loop will iterate through the list and compare each element to the next element.
+        # If the element is greater than the next element, it will swap the two elements.
+        # If the element is less than the next element, it will not swap the two elements.
+        # This will continue until the list is sorted.
+        for i in range(0, len(list) - 1):
+            if list[i] > list[i+1]:
+                sorted = False
+                list[i], list[i+1] = list[i+1], list[i]
+    
+    return list
 
-sorting(array)
+print(bubble(array))
