e5dd86ec9e18a2c00e9e501cf9f0ad6ae38326dc
MikkelHebel <mikkelhebelv2@gmail.com>
2023-08-30 23:13:11 +0200

    Made BFS file and learned about hashmaps :D

---
+++ Data Structures/hashmap.py
@@ -0,0 +1,12 @@
+# Initialise hashmap.
+city_map = {}
+
+# Set our cities (keys) as cities in canada.
+cities = ['Calgary', 'Vancouver', 'Toronto']
+
+# Append cities to our hashmap.
+city_map['Canada'] = []
+city_map['Canada'] += cities
+
+# Show our hash map.
+print(city_map)
---
+++ Searching Algorithms/Breadth_First_Search.py
@@ -0,0 +1,7 @@
+# Breadth First Search (BFS) is a graph traversal algorithm that explores a graph level by level, starting from a chosen vertex.
+# It visits all the neighbors of the starting vertex before moving on to their neighbors, ensuring that nodes at the same level are visited
+# before deeper levels. BFS is often used to find the shortest path between two vertices and to explore all reachable nodes in a graph.
+
+# Big O Notation of Breadth First Search:
+# O(V + E)
+# V represents the number of vertices and E represents the number of edges in the graph.
